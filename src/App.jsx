f576262f40b205f4ee6bd49a965c597689d90fa2
digvijaybhonsle <digvijaybhonsle007@gmail.com>
2025-07-03 19:58:11 +0530

    Fix: correct import path for Cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Cart from "./pages/cart";
+import Cart from "./pages/Cart";
 import About from "./pages/About";
 
 function App() {
